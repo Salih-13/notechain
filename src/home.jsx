8698c4818b26bfc31f77689955c304ddacf02763
Salih-13 <mohammedsalih050@gmail.com>
2024-07-13 19:14:12 +0530

    initial commit

---
+++ src/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div>
+      <div className='home'>
+        <img src=''></img>
+      </div>
+    </div>
+  )
+}
+
+export default home
